dc628ea9c5d16e6984230bb063f741fb7a4d63f1
Laiquah <cotielaiquah@gmail.com>
2023-08-14 14:40:24 +0200

    controller-user

--- api/model/BookAuthor.js
+++ api/model/BookAuthor.js
@@ -1,1 +1,6 @@
 // book authors
+class BookAuthor{
+
+}
+
+module.exports = BookAuthor
--- api/model/index.js
+++ api/model/index.js
@@ -1,1 +1,10 @@
 // import all models
+const Users = require('./Users')
+const Orders = require('./Orders')
+const Books = require('./Books')
+const BookAuthor = require('./BookAuthor')
+
+//export all objects
+module.exports = {
+    users: new Users(),
+}
