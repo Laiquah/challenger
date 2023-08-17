8b13794f43d45c0a6f1e515d470f61348ff68346
Laiquah <cotielaiquah@gmail.com>
2023-08-17 14:30:27 +0200

    extra

--- api/index.js
+++ api/index.js
@@ -177,25 +177,44 @@
 //   console.log(`Listening on ${port}`);
 // });
 
+const cookieParser = require("cookie-parser");
 const { express, routes } = require("./controller");
 const path = require("path");
+// const log = require('console')
+const errorHandling = require('./middleware/ErrorHandling')
+const cors = require("cors")
 const port = +process.env.PORT || 3000;
 const app = express();
+// log(" ")
 
-app.use(express.static("./static"));
+// app.use(express.static("./static"));
 
-app.use(express.urlencoded({ extended: false }), routes);
+app.use(
+  express.static('./static'),
+  express.urlencoded({ 
+    extended: false 
+  }),
+  cookieParser(),
+  cors(),
+  routes
+);
 
-routes.get(
-  "^/$|/challenger",
-  (req, res, next) => {
-    console.log("welcome back");
-  },
-  (req, res) => {
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Request-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Expose-Headers", "Authorization");
+  next();
+});
+
+routes.get("^/$|/challenger", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./static/html/index.html"));
   }
 );
 
+app.use(errorHandling)
 app.listen(port, () => {
   console.log(`You are listening on port ${port}`);
 });
